aeed974b967873206260f5fe01311878f2084624
manuel.casillas <manuel.casillas@infobric.com>
2024-01-17 20:53:51 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Amelia's baptism invitation project.
---
+++ js/clock.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+  let clock;
+
+  // Grab the current date
+  let currentDate = new Date();
+
+  // Target future date/24 hour time/Timezone
+  let targetDate = moment.tz("2024-03-24 10:30", "MST");
+
+  // Calculate the difference in seconds between the future and current date
+  let diff = targetDate / 1000 - currentDate.getTime() / 1000;
+
+  if (diff <= 0) {
+    // If remaining countdown is 0
+    clock = $(".clock").FlipClock(0, {
+      clockFace: "DailyCounter",
+      countdown: true,
+      language: "spanish",
+      autostart: false
+    });
+    console.log("Date has already passed!")
+    
+  } else {
+    // Run countdown timer
+    clock = $(".clock").FlipClock(diff, {
+      clockFace: "DailyCounter",
+      countdown: true,
+      language: "spanish",
+      callbacks: {
+        stop: function() {
+          console.log("Timer has ended!")
+        }
+      }
+    });
+    
+    // Check when timer reaches 0, then stop at 0
+    setTimeout(function() {
+      checktime();
+    }, 1000);
+    
+    function checktime() {
+      t = clock.getTime();
+      if (t <= 0) {
+        clock.setTime(0);
+      }
+      setTimeout(function() {
+        checktime();
+      }, 1000);
+    }
+  }
+});
